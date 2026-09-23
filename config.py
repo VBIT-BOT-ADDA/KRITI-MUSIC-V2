369d42b8da6194a029440c6e29af5095eefa7ecd
VBIT-BOT-ADDA <hakeebtravels@gmail.com>
2026-09-23 17:46:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID","-1003792391488"))
 # Get this value from @MissRose_Bot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID","6391774843"))
 
-API_URL = getenv("API_URL", 'http://16.16.151.215:8000') #youtube song url
-API_KEY = getenv("API_KEY", 'Darkbit_sXDHz5MjMS4g8SH2fH72_IRim9PYfIRy') # youtube song api ke # -------------------------------------------------------------------------#
+ARC_API_URL = getenv("ARC_API_URL", "https://api.arcmusic.fun")
+ARC_API_KEY = getenv("ARC_API_KEY", "ARC5c354b8ef8b2b1ef5d9e8b")
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
