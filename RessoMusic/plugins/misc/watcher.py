08e66204d9187c260ca2d6d8acfd1104cdb30611
VBIT-BOT-ADDA <hakeebtravels@gmail.com>
2026-09-23 23:08:14 +0530

    Update watcher.py

--- RessoMusic/plugins/misc/watcher.py
+++ RessoMusic/plugins/misc/watcher.py
@@ -11,4 +11,7 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await AMBOTOP.stop_stream_force(message.chat.id)
+    try:
+        await AMBOTOP.stop_stream(message.chat.id)
+    except Exception:
+        pass
